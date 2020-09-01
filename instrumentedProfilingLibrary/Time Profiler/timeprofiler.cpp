51cd9483faa3dad5189755d35413290a7b993f46
Ed Amutete <53301147+ed-learner@users.noreply.github.com>
2020-09-01 16:51:58 +0200

    update time_profiler.cpp

--- instrumentedProfilingLibrary/Time Profiler/timeprofiler.cpp
+++ instrumentedProfilingLibrary/Time Profiler/timeprofiler.cpp
@@ -7,8 +7,8 @@ using namespace std;
 time_profiler::time_profiler(string blockName):codeBlock(move(blockName)), codeBlockStartTime(chrono::high_resolution_clock::now()){
 	cout << "time profiler constructor called" << endl;
 	elapsedTime = 90000;
-	//copyofcodeBlockStartTime = codeBlockStartTime;
-
+	
+	constructorProcesses();
 }
 
 time_profiler::~time_profiler(){
@@ -16,7 +16,24 @@ time_profiler::~time_profiler(){
 	codeBlockExecutionTime = chrono::duration_cast<chrono::microseconds>(codeBlockEndTime - codeBlockStartTime);
 	cout << codeBlock << " execution time elapsed: " << codeBlockExecutionTime.count() << " microseconds" << endl;
 	cout << "time profiler destructor called\n" << endl;
-	//copyofcodeBlockEndTime = codeBlockEndTime;
-	//copyofcodeBlockExecutionTime = codeBlockExecutionTime;
+	
+	destructorProcesses();
 }
 
+void time_profiler::constructorProcesses() {
+	StartTimeStamp_us = chrono::duration_cast<chrono::microseconds>(codeBlockStartTime.time_since_epoch()).count();
+	auto StartTimeStamp_ns = chrono::duration_cast<chrono::nanoseconds>(codeBlockStartTime.time_since_epoch()).count();
+	cout << StartTimeStamp_us << endl;
+	cout << StartTimeStamp_ns << endl;
+
+}
+
+void time_profiler::destructorProcesses() {
+	EndTimeStamp_us = chrono::duration_cast<chrono::microseconds>(codeBlockEndTime.time_since_epoch()).count();
+	auto EndTimeStamp_ns = chrono::duration_cast<chrono::nanoseconds>(codeBlockEndTime.time_since_epoch()).count();
+
+	BlockExecutionTime_us = codeBlockExecutionTime.count();
+	auto BlockExecutionTime_ns = chrono::duration_cast<chrono::nanoseconds>(codeBlockExecutionTime).count();
+
+	//cout << codeBlock << " execution time elapsed from epochs: " << EndTimeStamp_us - StartTimeStamp_us << " microseconds" << endl
+}
