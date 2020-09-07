96352bb13cd7da8af0a7a88b648606a25a4f04f7
Ed Amutete <53301147+ed-learner@users.noreply.github.com>
2020-09-07 11:55:41 +0200

    update timeprofiler.hh

--- instrumentedProfilingLibrary/Time Profiler/timeprofiler.hh
+++ instrumentedProfilingLibrary/Time Profiler/timeprofiler.hh
@@ -1,25 +1,12 @@
 #pragma once
 
-#include<iostream>
 #include <chrono>
-
 
 class time_profiler{
 	public:
-		time_profiler() = default;
 		time_profiler(std::string blockName);
 		~time_profiler();
-	         
-		void constructorProcesses();
-		void destructorProcesses();
-
-		//time stamp in microseconds.
-		uint64_t StartTimeStamp_us;
-		uint64_t EndTimeStamp_us;
-		uint64_t BlockExecutionTime_us;
 	
-		int elapsedTime;
-
 	private:
 		std::string codeBlock;
 		std::chrono::time_point<std::chrono::high_resolution_clock> codeBlockStartTime;
