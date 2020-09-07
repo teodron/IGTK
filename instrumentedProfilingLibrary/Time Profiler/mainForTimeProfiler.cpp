9612d3b7e97abb63f01dafa6bf81d6032123d9b1
Ed Amutete <53301147+ed-learner@users.noreply.github.com>
2020-09-07 12:37:20 +0200

    Update mainForTimeProfiler.cpp

--- instrumentedProfilingLibrary/Time Profiler/mainForTimeProfiler.cpp
+++ instrumentedProfilingLibrary/Time Profiler/mainForTimeProfiler.cpp
@@ -7,16 +7,26 @@ using namespace std;
 
 int main() {
 
-	//operations op;
-	ProfileDataManager dm;
+	operations op;
+	ProfileDataManager* tp = ProfileDataManager::GetInstance("code block");
 
 	{
-		//time_profiler time_profiler("sum code block");
-		//op.sumOfNumbers(1000);
-		dm.managerMain("sum code block");
+		time_profiler time_profiler("sum code block");
+		op.sumOfNumbers(1000);
+		
+	}
+         
+	tp->displayTimerData();
+	tp->storeTimerData();
+	
+	{
+		time_profiler time_profiler("square root code block");
+		op.sqrtOfNumbersSum(10000);
 
 	}
 
+	tp->displayTimerData();
+	tp->storeTimerData();
 
 	return 0;
 
